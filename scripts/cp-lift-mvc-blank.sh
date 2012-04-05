d1f614b881e2c5158e3a1dd96ced0d0e842ce788
Byron Gibson <fbg111@gmail.com>
2012-04-05 15:58:59 -0700

    init

---
+++ scripts/cp-lift-mvc-blank.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#cp -r ~/bin/projects/lift/lift-stack/lift-24-sbt/scala_29/lift_blank/* ./lift-24-s29-blank.g8
+#cp -r ~/bin/projects/lift/lift-stack/lift-24-sbt/scala_29/lift_blank/* ./lift-24-s29-html5bp.g8
+#cp -r ~/bin/projects/lift/lift-stack/lift-24-sbt/scala_29/lift_blank/* ./lift-24-s29-html5bp-bootstrap.g8
+#cp -r ~/bin/projects/lift/lift-stack/lift-24-sbt/scala_29/lift_blank/* ./lift-24-s29-html5bp-kickstrap.g8
+
+cp -r ~/bin/projects/lift/lift-stack/lift-24-sbt/scala_29/lift_mvc/* lift24-s29-mvc-blank.g8
+cp -r ~/bin/projects/lift/lift-stack/lift-24-sbt/scala_29/lift_mvc/* lift24-s29-mvc-html5bp.g8
+cp -r ~/bin/projects/lift/lift-stack/lift-24-sbt/scala_29/lift_mvc/* lift24-s29-mvc-html5bp-bootstrap.g8
+cp -r ~/bin/projects/lift/lift-stack/lift-24-sbt/scala_29/lift_mvc/* lift24-s29-mvc-html5bp-kickstrap.g8
